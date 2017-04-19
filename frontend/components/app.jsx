7e5379a0e66be0a9ef61ef8475604936222d6e13
Christine Wang <christinewang319@gmail.com>
2017-04-18 20:29:27 -0400

    Add app and root components

---
+++ frontend/components/app.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const App = ({ children }) => {
+  <div>
+    <h1>EtsAClone</h1>
+    { children }
+  </div>
+};
+
+export default App;
---
+++ frontend/components/root.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { Router, Route, IndexRoute, hashHistory } from 'react-router';
+
+const Root = ({store}) => (
+  <Provider store={ store }>
+    <Router history={ hashHistory }>
+      <Route path='/' component={ App } />
+    </Router>
+  </Provider>
+);
