1d48a6b8a370529320725dcc7a572df1e584c140
Christine Wang <christinewang319@gmail.com>
2017-04-19 10:21:33 -0400

    Protect front-end routers with onEnter hooks

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -5,14 +5,31 @@ import { Router, Route, IndexRoute, hashHistory } from 'react-router';
 import App from './app';
 import SessionFormContainer from './session_form/session_form_container';
 
-const Root = ({store}) => (
-  <Provider store={ store }>
-    <Router history={ hashHistory }>
-      <Route path='/' component={ App } />
-        <Route path='/register' component={SessionFormContainer} />
-        <Route path='/signin' component={SessionFormContainer} />
-    </Router>
-  </Provider>
-);
+const Root = ({ store }) => {
+  const _ensureSignedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+
+    if (!currentUser) {
+      replace('/signin');
+    }
+  };
+
+  const _redirectIfSignedIn = (nextState, replace) => {
+    const currentUser = store.getState().session.currentUser;
+    if (currentUser) {
+      replace('/');
+    }
+  }
+
+  return (
+    <Provider store={ store }>
+      <Router history={ hashHistory }>
+        <Route path='/' component={ App } />
+        <Route path='/register' component={ SessionFormContainer } onEnter={ _redirectIfSignedIn } />
+        <Route path='/signin' component={ SessionFormContainer } onEnter={ _redirectIfSignedIn } />
+      </Router>
+    </Provider>
+  );
+};
 
 export default Root;
