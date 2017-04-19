f5b165318979548e194ca23b07197fe09be61d13
Christine Wang <christinewang319@gmail.com>
2017-04-19 10:02:35 -0400

    Add preloaded state to store

--- frontend/etsaclone.jsx
+++ frontend/etsaclone.jsx
@@ -5,8 +5,15 @@ import configureStore from './store/store';
 import Root from './components/root';
 
 document.addEventListener('DOMContentLoaded', () => {
-  const store = configureStore();
-  const root = document.getElementById('root');
+  let store;
 
-  ReactDOM.render(<Root store={ store }/>, root);
+  if (window.currentUser) {
+    const preloadedState = { session: { currentUser: window.currentUser } };
+    store = configureStore(preloadedState);
+  } else {
+    store = configureStore();
+  }
+
+  const root = document.getElementById('root');
+  ReactDOM.render(<Root store={store} />, root);
 });
