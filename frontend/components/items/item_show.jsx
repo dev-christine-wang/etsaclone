67bd6236ec2e7822a8ce164758441f301cffc52b
Christine Wang <christinewang319@gmail.com>
2017-04-22 18:00:20 -0400

    Make changes to item show component

--- frontend/components/items/item_show.jsx
+++ frontend/components/items/item_show.jsx
@@ -10,15 +10,18 @@ class ItemShow extends Component {
     const { item } = this.props;
 
     return (
-      <div>
-        <ul>
-          <li>
-            <img src={ item.image } alt={ item.name } />
-          </li>
-          <li>{ item.name }</li>
-          <li>{ `$${item.price}.00` }</li>
-          <li>{ item.description }</li>
-        </ul>
+      <div className='item-show'>
+        <div className='item-show-image'>
+          <img src={ item.image } alt={ item.name } />
+        </div>
+        <div className='item-show-details'>
+          <ul>
+            <li>{ item.name }</li>
+            <li className='item-show-seller'>{ `${item.seller.first_name} ${item.seller.last_name}` }</li>
+            <li className='item-show-price'>{ `$${item.price}.00` }</li>
+            <li>{ item.description }</li>
+          </ul>
+        </div>
       </div>
     );
   }
