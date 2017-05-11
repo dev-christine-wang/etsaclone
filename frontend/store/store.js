4b86c6f6774f88843d5687e7fc9f1ffeda9873ff
Christine Wang <christinewang319@gmail.com>
2017-05-11 11:41:49 -0400

    Remove react and redux minification warnings

--- frontend/store/store.js
+++ frontend/store/store.js
@@ -4,12 +4,14 @@ import logger from 'redux-logger';
 
 import RootReducer from '../reducers/root_reducer';
 
+const middlewares = [thunk];
+
+if (process.env.NODE_ENV !== 'production') {
+  middlewares.push(logger);
+}
+
 const configureStore = (preloadedState = {}) => (
-  createStore(
-    RootReducer,
-    preloadedState,
-    applyMiddleware(thunk, logger)
-  )
-)
+  createStore(RootReducer, preloadedState, applyMiddleware(...middlewares))
+);
 
 export default configureStore;
