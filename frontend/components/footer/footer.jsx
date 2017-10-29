c3a4bbbb411486e0e6a4fc388eaaab24558086f7
Christine Wang <christinewang319@gmail.com>
2017-10-28 23:14:47 -0400

    refactor(footer)

--- frontend/components/footer/footer.jsx
+++ frontend/components/footer/footer.jsx
@@ -3,20 +3,16 @@ import { Link } from 'react-router';
 
 const Footer = () => (
   <div className='footer'>
-    <ul className='my-info'>
-      <li>Project by Christine Wang</li>
-    </ul>
+    <div className='my-info'>Project by Christine Wang</div>
     <ul className='links'>
-      <li className='github'>
+      <li>
         <a href={'https://github.com/christinewang319'}>Github</a>
       </li>
       <li>
         <a href={'https://www.linkedin.com/in/christinewang319/'}>LinkedIn</a>
       </li>
     </ul>
-    <ul className='disclaimer'>
-      <li>Disclaimer: For educational purposes only.</li>
-    </ul>
+    <div className='disclaimer'>Disclaimer: For educational purposes only.</div>
   </div>
 );
 
