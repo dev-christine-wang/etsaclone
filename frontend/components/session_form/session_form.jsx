a2d7a8c7455ad456b5749b4cf137f3f0dace45d4
Christine Wang <christinewang319@gmail.com>
2017-10-29 14:53:17 -0400

    refactor(session_form)

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -58,98 +58,94 @@ class SessionForm extends React.Component {
   render() {
     if (this.props.formType === 'register') {
       return (
-        <div className='register-form-container'>
-          <form onSubmit={this.handleSubmit} className='register-form-box'>
-            <div className='register-message'>
-              <span>Create an EtsAClone account and start shopping</span>
-            </div>
-            <div className='register-form'>
-              <label>First Name
-                <br/>
-                <input id='form-field'
-                        type='text'
-                        value={this.state.first_name}
-                        onChange={this.update('first_name')}
-                        className='register-input' />
-                &nbsp;&nbsp;
-                <span>{this.props.errors.first_name}</span>
-              </label>
+        <form className='register-form-box' onSubmit={this.handleSubmit}>
+          <div className='register-message'>
+            <span>Create an EtsAClone account and start shopping</span>
+          </div>
+          <div className='register-form'>
+            <label>First Name
               <br/>
-              <label>Last Name
-                <br/>
-                <input id='form-field'
-                        type='text'
-                        value={this.state.last_name}
-                        onChange={this.update('last_name')}
-                        className='register-input' />
-                &nbsp;&nbsp;
-                <span>{this.props.errors.last_name}</span>
-              </label>
+              <input id='form-field'
+                      type='text'
+                      value={this.state.first_name}
+                      onChange={this.update('first_name')}
+                      className='register-input' />
+              &nbsp;&nbsp;
+              <span>{this.props.errors.first_name}</span>
+            </label>
+            <br/>
+            <label>Last Name
               <br/>
-              <label>Email
-                <br/>
-                <input id='form-field'
-                        type='text'
-                        value={this.state.email}
-                        onChange={this.update('email')}
-                        className='register-input' />
-                &nbsp;&nbsp;
-                <span>{this.props.errors.email}</span>
-              </label>
+              <input id='form-field'
+                      type='text'
+                      value={this.state.last_name}
+                      onChange={this.update('last_name')}
+                      className='register-input' />
+              &nbsp;&nbsp;
+              <span>{this.props.errors.last_name}</span>
+            </label>
+            <br/>
+            <label>Email
               <br/>
-              <label>Password
-                <br/>
-                <input id='form-field'
-                        type='password'
-                        value={this.state.password}
-                        onChange={this.update('password')}
-                        className='register-input' />
-                &nbsp;&nbsp;
-                <span>{this.props.errors.password}</span>
-              </label>
+              <input id='form-field'
+                      type='text'
+                      value={this.state.email}
+                      onChange={this.update('email')}
+                      className='register-input' />
+              &nbsp;&nbsp;
+              <span>{this.props.errors.email}</span>
+            </label>
+            <br/>
+            <label>Password
               <br/>
-              <input type='submit' value='Register' />
-              <br/>
-              <p>By clicking Register, you agree to consider hiring me.</p>
-            </div>
-          </form>
-        </div>
+              <input id='form-field'
+                      type='password'
+                      value={this.state.password}
+                      onChange={this.update('password')}
+                      className='register-input' />
+              &nbsp;&nbsp;
+              <span>{this.props.errors.password}</span>
+            </label>
+            <br/>
+            <input type='submit' value='Register' />
+            <br/>
+            <p>By clicking Register, you agree to consider hiring me.</p>
+          </div>
+        </form>
       );
     } else {
       return (
-        <div className='signin-form-container'>
-          <form onSubmit={this.handleSubmit} className='signin-form-box'>
-            <div className='sign-in-message'>
-              <span>Sign into EtsAClone</span>
-            </div>
-            <div className='signin-form'>
-              <label>Email
-                <br/>
-                <input id='form-field'
-                        type='text'
-                        value={this.state.email}
-                        onChange={this.update('email')}
-                        className='signin-input' />
-                &nbsp;&nbsp;
-                <span>{this.props.errors.base}</span>
-              </label>
+        <form onSubmit={this.handleSubmit} className='signin-form-box'>
+          <div className='sign-in-message'>
+            <span>Sign into EtsAClone</span>
+          </div>
+          <div className='signin-form'>
+            <label>Email
               <br/>
-              <label>Password
-                <br/>
-                <input id='form-field'
-                        type='password'
-                        value={this.state.password}
-                        onChange={this.update('password')}
-                        className='signin-input' />
-                &nbsp;&nbsp;
-                <span>{this.props.errors.base}</span>
-              </label>
+              <input id='form-field'
+                      type='text'
+                      value={this.state.email}
+                      onChange={this.update('email')}
+                      className='signin-input' />
+              &nbsp;&nbsp;
+              <span>{this.props.errors.base}</span>
+            </label>
+            <br/>
+            <label>Password
               <br/>
-              <input type='submit' value='Sign In' />
-              <br/>
-            </div>
-          </form>
-        </div>
+              <input id='form-field'
+                      type='password'
+                      value={this.state.password}
+                      onChange={this.update('password')}
+                      className='signin-input' />
+              &nbsp;&nbsp;
+              <span>{this.props.errors.base}</span>
+            </label>
+            <br/>
+            <input type='submit' value='Sign In' />
+            <br/>
+          </div>
+        </form>
       );
     }
   }
