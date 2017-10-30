1e8ef47fdf120fbc2e56c30abc9db55d5109564f
Christine Wang <christinewang319@gmail.com>
2017-10-30 00:06:22 -0400

    feat(categories): add earrings, necklaces, and rings categories

---
+++ frontend/components/items/item_category_index.jsx
@@ -0,0 +1,51 @@
+import React, { Component } from 'react';
+import ItemIndexItem from './item_index_item';
+
+export class ItemCategoryIndex extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { category: null };
+  }
+
+  componentDidMount() {
+    const categories = this.props.location.pathname.split('/');
+    const category = categories[categories.length - 1];
+
+    this.props.fetchSearchedItems(category);
+    this.setState({ category });
+  }
+
+  componentWillReceiveProps(nextProps) {
+    if (this.props.location.pathname !== nextProps.location.pathname) {
+      const categories = nextProps.location.pathname.split('/');
+      const category = categories[categories.length - 1];
+
+      this.props.fetchSearchedItems(category);
+      this.setState({ category });
+    }
+  }
+
+  render() {
+    const { items } = this.props;
+
+    if (items) {
+      const sectionHeaders = { earrings: 'Earrings', necklace: 'Necklaces', ring: 'Rings' };
+      const sectionHeader = sectionHeaders[this.state.category];
+
+      return (
+        <div className='all-items-index'>
+          <h2>{ sectionHeader }</h2>
+          <div className='all-items'>
+            <ul>
+              { items.map(item => <ItemIndexItem key={item.id} item={item} />)}
+            </ul>
+          </div>
+        </div>
+      )
+    }
+
+    return (
+      <div></div>
+    )
+  }
+}
